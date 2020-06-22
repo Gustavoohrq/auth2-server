0ffabfe3c730cd3706378108c0785630e5f6696b
Gustavo Henrique <gustavofmariano@gmail.com>
2020-06-22 11:44:13 -0300

    ...

--- db/clients.js
+++ db/clients.js
@@ -1,40 +1,10 @@
 'use strict';
 
-/**
- * This is the configuration of the clients that are allowed to connected to your authorization
- * server. These represent client applications that can connect. At a minimum you need the required
- * properties of
- *
- * id:           A unique numeric id of your client application
- * name:         The name of your client application
- * clientId:     A unique id of your client application
- * clientSecret: A unique password(ish) secret that is _best not_ shared with anyone but your
- *               client application and the authorization server.
- *
- * Optionally you can set these properties which are
- *
- * trustedClient: default if missing is false. If this is set to true then the client is regarded
- * as a trusted client and not a 3rd party application. That means that the user will not be
- * presented with a decision dialog with the trusted application and that the trusted application
- * gets full scope access without the user having to make a decision to allow or disallow the scope
- * access.
- */
 const clients = [{
   id            : '1',
-  name          : 'Samplr',
-  clientId      : 'abc123',
-  clientSecret  : 'ssh-secret',
-}, {
-  id            : '2',
-  name          : 'Samplr2',
-  clientId      : 'xyz123',
-  clientSecret  : 'ssh-password',
-}, {
-  id            : '3',
-  name          : 'Samplr3',
-  clientId      : 'trustedClient',
-  clientSecret  : 'ssh-otherpassword',
-  trustedClient : true,
+  name          : 'Lyceum OAuth2.0',
+  clientId      : '45f501943e9970a27e32c5f14990d79f',
+  clientSecret  : '34114a83504043a492f9e497958e19f9',
 }];
 
 /**
