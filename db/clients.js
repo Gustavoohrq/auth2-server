e673e53cb3c3bd894dd4b8bdd5d9622981bc874d
Gustavo Henrique <gustavofmariano@gmail.com>
2020-06-24 17:28:46 -0300

    add client schema

--- db/clients.js
+++ db/clients.js
@@ -1,24 +1,20 @@
 'use strict';
 
-const clients = [{
-  id            : '1',
-  name          : 'Lyceum OAuth2.0',
-  clientId      : '45f501943e9970a27e32c5f14990d79f',
-  clientSecret  : '34114a83504043a492f9e497958e19f9',
-}];
+const connection = require("./connection");
 
-/**
- * Returns a client if it finds one, otherwise returns null if a client is not found.
- * @param   {String}   id   - The unique id of the client to find
- * @returns {Promise}  resolved promise with the client if found, otherwise undefined
- */
-exports.find = id => Promise.resolve(clients.find(client => client.id === id));
+exports.find = id => {
+  connection.query('SELECT * FROM client where id=?', id, function (error, results, fields) {
+    if (error) return done(new Error('Client not found'))
+    else if (results[0] === '' || results[0] === undefined || results[0] === null) return done(new Error('Client not found'))
+    else return done(null, results[0])
+  })
+}
 
-/**
- * Returns a client if it finds one, otherwise returns null if a client is not found.
- * @param   {String}   clientId - The unique client id of the client to find
- * @param   {Function} done     - The client if found, otherwise returns undefined
- * @returns {Promise} resolved promise with the client if found, otherwise undefined
- */
-exports.findByClientId = clientId =>
-  Promise.resolve(clients.find(client => client.clientId === clientId));
+
+exports.findByClientId = (clientId, done) => {
+  connection.query('SELECT * FROM client where clientId=?', clientId, function (error, results, fields) {
+    if (error) return done(new Error('Client not found'))
+    else if (results[0] === '' || results[0] === undefined || results[0] === null) return done(new Error('Client not found'))
+    else return done(null, results[0])
+  })
+}
