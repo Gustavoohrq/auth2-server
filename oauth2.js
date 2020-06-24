632add7d4cdec2458713ef7e5b5d9bd6252a6b50
Gustavo Henrique <gustavofmariano@gmail.com>
2020-06-24 17:29:42 -0300

    refactor user authorization endpoint

--- oauth2.js
+++ oauth2.js
@@ -7,19 +7,19 @@
 // through a process of the user granting access, and the client exchanging
 // the grant for an access token.
 
-const config      = require('./config');
-const db          = require('./db');
-const login       = require('connect-ensure-login');
+const config = require('./config');
+const db = require('./db');
+const login = require('connect-ensure-login');
 const oauth2orize = require('oauth2orize');
-const passport    = require('passport');
-const utils       = require('./utils');
-const validate    = require('./validate');
+const passport = require('passport');
+const utils = require('./utils');
+const validate = require('./validate');
 
 // create OAuth 2.0 server
 const server = oauth2orize.createServer();
 
 // Configured expiresIn
-const expiresIn = { expires_in : config.token.expiresIn };
+const expiresIn = { expires_in: config.token.expiresIn };
 
 /**
  * Grant authorization codes
@@ -31,10 +31,10 @@ const expiresIn = { expires_in : config.token.expiresIn };
  * which is bound to these values, and will be exchanged for an access token.
  */
 server.grant(oauth2orize.grant.code((client, redirectURI, user, ares, done) => {
-  const code = utils.createToken({ sub : user.id, exp : config.codeToken.expiresIn });
+  const code = utils.createToken({ sub: user.id, exp: config.codeToken.expiresIn });
   db.authorizationCodes.save(user, code, client.id, redirectURI, user.id, client.scope)
-  .then(() => done(null, code))
-  .catch(err => done(err));
+    .then(() => done(null, code))
+    .catch(err => done(err));
 }));
 
 /**
@@ -46,11 +46,11 @@ server.grant(oauth2orize.grant.code((client, redirectURI, user, ares, done) => {
  * which is bound to these values.
  */
 server.grant(oauth2orize.grant.token((client, user, ares, done) => {
-  const token      = utils.createToken({ sub : user.id, exp : config.token.expiresIn });
+  const token = utils.createToken({ sub: user.id, exp: config.token.expiresIn });
   const expiration = config.token.calculateExpirationDate();
   db.accessTokens.save(token, expiration, user.id, client.id, client.scope)
-  .then(() => done(null, token, expiresIn))
-  .catch(err => done(err));
+    .then(() => done(null, token, expiresIn))
+    .catch(err => done(err));
 }));
 
 /**
@@ -64,18 +64,18 @@ server.grant(oauth2orize.grant.token((client, user, ares, done) => {
 server.exchange(oauth2orize.exchange.code((client, code, redirectURI, done) => {
 
   db.authorizationCodes.delete(code)
-  .then(authCode => validate.authCode(code, authCode, client, redirectURI))
-  .then(authCode =>validate.generateTokens(authCode))
-  .then((tokens) => {
-    if (tokens.length === 1) {
-      return done(null, tokens[0], null, expiresIn);
-    }
-    if (tokens.length === 2) {
-      return done(null, tokens[0], tokens[1], expiresIn);
-    }
-    throw new Error('Error exchanging auth code for tokens');
-  })
-  .catch(() => done(null, false));
+    .then(authCode => validate.authCode(code, authCode, client, redirectURI))
+    .then(authCode => validate.generateTokens(authCode))
+    .then((tokens) => {
+      if (tokens.length === 1) {
+        return done(null, tokens[0], null, expiresIn);
+      }
+      if (tokens.length === 2) {
+        return done(null, tokens[0], tokens[1], expiresIn);
+      }
+      throw new Error('Error exchanging auth code for tokens');
+    })
+    .catch(() => done(null, false));
 }));
 
 /**
@@ -87,20 +87,20 @@ server.exchange(oauth2orize.exchange.code((client, code, redirectURI, done) => {
  */
 server.exchange(oauth2orize.exchange.password((client, username, password, scope, done) => {
   db.users.findByUsername(username, password)
-  .then(user => validate.generateTokens({ scope, userID: user.id, clientID: client.id }))
-  .then((tokens) => {
-    if (tokens === false) {
-      return done(null, false);
-    }
-    if (tokens.length === 1) {
-      return done(null, tokens[0], null, expiresIn);
-    }
-    if (tokens.length === 2) {
-      return done(null, tokens[0], tokens[1], expiresIn);
-    }
-    throw new Error('Error exchanging password for tokens');
-  })
-  .catch(() => done(null, false));
+    .then(user => validate.generateTokens({ scope, userID: user.id, clientID: client.id }))
+    .then((tokens) => {
+      if (tokens === false) {
+        return done(null, false);
+      }
+      if (tokens.length === 1) {
+        return done(null, tokens[0], null, expiresIn);
+      }
+      if (tokens.length === 2) {
+        return done(null, tokens[0], tokens[1], expiresIn);
+      }
+      throw new Error('Error exchanging password for tokens');
+    })
+    .catch(() => done(null, false));
 }));
 
 /**
@@ -111,12 +111,12 @@ server.exchange(oauth2orize.exchange.password((client, username, password, scope
  * application issues an access token on behalf of the client who authorized the code.
  */
 server.exchange(oauth2orize.exchange.clientCredentials((client, scope, done) => {
-  const token      = utils.createToken({ sub : client.id, exp : config.token.expiresIn });
+  const token = utils.createToken({ sub: client.id, exp: config.token.expiresIn });
   const expiration = config.token.calculateExpirationDate();
   // Pass in a null for user id since there is no user when using this grant type
   db.accessTokens.save(token, expiration, null, client.id, scope)
-  .then(() => done(null, token, null, expiresIn))
-  .catch(err => done(err));
+    .then(() => done(null, token, null, expiresIn))
+    .catch(err => done(err));
 }));
 
 /**
@@ -128,71 +128,41 @@ server.exchange(oauth2orize.exchange.clientCredentials((client, scope, done) => 
  */
 server.exchange(oauth2orize.exchange.refreshToken((client, refreshToken, scope, done) => {
   db.refreshTokens.find(refreshToken)
-  .then(foundRefreshToken => validate.refreshToken(foundRefreshToken, refreshToken, client))
-  .then(foundRefreshToken => validate.generateToken(foundRefreshToken))
-  .then(token => done(null, token, null, expiresIn))
-  .catch(() => done(null, false));
+    .then(foundRefreshToken => validate.refreshToken(foundRefreshToken, refreshToken, client))
+    .then(foundRefreshToken => validate.generateToken(foundRefreshToken))
+    .then(token => done(null, token, null, expiresIn))
+    .catch(() => done(null, false));
 }));
 
 /*
  * User authorization endpoint
- *
- * `authorization` middleware accepts a `validate` callback which is
- * responsible for validating the client making the authorization request.  In
- * doing so, is recommended that the `redirectURI` be checked against a
- * registered value, although security requirements may vary accross
- * implementations.  Once validated, the `done` callback must be invoked with
- * a `client` instance, as well as the `redirectURI` to which the user will be
- * redirected after an authorization decision is obtained.
- *
- * This middleware simply initializes a new authorization transaction.  It is
- * the application's responsibility to authenticate the user and render a dialog
- * to obtain their approval (displaying details about the client requesting
- * authorization).  We accomplish that here by routing through `ensureLoggedIn()`
- * first, and rendering the `dialog` view.
  */
+
 exports.authorization = [
   login.ensureLoggedIn(),
   server.authorization((clientID, redirectURI, scope, done) => {
-    db.clients.findByClientId(clientID)
-    .then((client) => {
+    db.clients.findByClientId(clientID, (error, client) => {
+      if (error) return done(error);
       if (client) {
-        client.scope = scope; // eslint-disable-line no-param-reassign
+        client.scope = scope;
       }
-      // WARNING: For security purposes, it is highly advisable to check that
-      //          redirectURI provided by the client matches one registered with
-      //          the server.  For simplicity, this example does not.  You have
-      //          been warned.
       return done(null, client, redirectURI);
-    })
-    .catch(err => done(err));
+    });
   }), (req, res, next) => {
-    // Render the decision dialog if the client isn't a trusted client
-    // TODO:  Make a mechanism so that if this isn't a trusted client, the user can record that
-    // they have consented but also make a mechanism so that if the user revokes access to any of
-    // the clients then they will have to re-consent.
-    db.clients.findByClientId(req.query.client_id)
-    .then((client) => {
-      if (client != null && client.trustedClient && client.trustedClient === true) {
-        // This is how we short call the decision like the dialog below does
+    db.clients.findByClientId(req.query.client_id, (error, client) => {
+      if (error) res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client })
+      if (client != null) {
         server.decision({ loadTransaction: false }, (serverReq, callback) => {
           callback(null, { allow: true });
         })(req, res, next);
       } else {
         res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client });
       }
-    })
-    .catch(() =>
-      res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client }));
+    });
   }];
 
 /**
  * User decision endpoint
- *
- * `decision` middleware processes a user's decision to allow or deny access
- * requested by a client application.  Based on the grant type requested by the
- * client, the above grant middleware configured above will be invoked to send
- * a response.
  */
 exports.decision = [
   login.ensureLoggedIn(),
@@ -201,12 +171,8 @@ exports.decision = [
 
 /**
  * Token endpoint
- *
- * `token` middleware handles client requests to exchange authorization grants
- * for access tokens.  Based on the grant type being exchanged, the above
- * exchange middleware will be invoked to handle the request.  Clients must
- * authenticate when making requests to this endpoint.
  */
+
 exports.token = [
   passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
   server.token(),
@@ -214,23 +180,12 @@ exports.token = [
 ];
 
 // Register serialialization and deserialization functions.
-//
-// When a client redirects a user to user authorization endpoint, an
-// authorization transaction is initiated.  To complete the transaction, the
-// user must authenticate and approve the authorization request.  Because this
-// may involve multiple HTTPS request/response exchanges, the transaction is
-// stored in the session.
-//
-// An application must supply serialization functions, which determine how the
-// client object is serialized into the session.  Typically this will be a
-// simple matter of serializing the client's ID, and deserializing by finding
-// the client by ID from the database.
 
 server.serializeClient((client, done) => done(null, client.id));
 
 server.deserializeClient((id, done) => {
   db.clients.find(id)
-  .then(client => done(null, client))
-  .catch(err => done(err));
+    .then(client => done(null, client))
+    .catch(err => done(err));
 });
 
